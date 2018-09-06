dc4782f8493a6f0998e734a1ed7af9efbcbbd07b
data-kid <gabriel.grover@gmail.com>
2018-09-05 18:42:05 -0700

    remove superluous import

--- src/contexts/CMS/index.js
+++ src/contexts/CMS/index.js
@@ -1,5 +1,5 @@
 const { to } = require("await-to-js");
-const { PollErrs, INVALID_ID } = require("../../errors/error_types");
+const { PollErrs } = require("../../errors/error_types");
 const Poll = require("../../models/poll");
 const Accounts = require("../accounts");
 
