782ef0be3a661c5c806fd7e6789b7d9bd7496146
data-kid <gabriel.grover@gmail.com>
2018-09-13 10:19:32 -0700

    update user validations

--- src/schemas/user/validations.js
+++ src/schemas/user/validations.js
@@ -2,17 +2,6 @@ const reg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
 
 exports.passwordREGEX = reg;
 
-exports.validatePassword = function(pw) {
-  if (pw.length < 8) {
-    return Promise.reject("Password must be atleast 8 characters.")
-  } else if (!pw.match(reg)) {
-    const msg = "Password must contain atleast one uppercase letter, one lowercase letter, one number and one special character."; 
-    return Promise.reject(msg);
-  } else {
-    return Promise.resolve(pw);
-  }
-};
-
 exports.validatePasswordLength = pw => pw.length >= 8;
 
 exports.validatePasswordFormat = pw => pw.match(reg);
